8fc33fddfa91259cba5efba952760e8bb5aff48e
Kalp Kanungo <kalpkanungo@Kalps-MacBook-Air.local>
2026-02-19 21:00:25 +0530

    initial commit

--- backend/model.py
+++ backend/model.py
@@ -1,23 +1,17 @@
 from transformers import MarianMTModel, MarianTokenizer
 
 
-def load_models():
+from transformers import MarianMTModel, MarianTokenizer
 
+def load_models():
     models = {}
 
-
-    en_de_path = "./models/marian_finetuned_20k_2ep"
-
-    tokenizer_en_de = MarianTokenizer.from_pretrained(en_de_path)
-    model_en_de = MarianMTModel.from_pretrained(en_de_path)
-
+    tokenizer_en_de = MarianTokenizer.from_pretrained("kalpkanungo/marian-en-de")
+    model_en_de = MarianMTModel.from_pretrained("kalpkanungo/marian-en-de")
     models["en-de"] = (tokenizer_en_de, model_en_de)
 
-    de_en_name = "Helsinki-NLP/opus-mt-de-en"
-
-    tokenizer_de_en = MarianTokenizer.from_pretrained(de_en_name)
-    model_de_en = MarianMTModel.from_pretrained(de_en_name)
-
+    tokenizer_de_en = MarianTokenizer.from_pretrained("kalpkanungo/marian-de-en")
+    model_de_en = MarianMTModel.from_pretrained("kalpkanungo/marian-de-en")
     models["de-en"] = (tokenizer_de_en, model_de_en)
 
     return models
--- check.py
+++ check.py
@@ -1,4 +1,15 @@
-from backend import translator
+from transformers import MarianMTModel, MarianTokenizer
 
-output=translator.translate("Hello my name is Kalp I am 20 years old and I live in Mumbai")
-print(output)
+# Push your fine-tuned EN->DE model
+model = MarianMTModel.from_pretrained("/Users/kalpkanungo/Desktop/English_German/models/marian_finetuned_20k_2ep")
+tokenizer = MarianTokenizer.from_pretrained("/Users/kalpkanungo/Desktop/English_German/models/marian_finetuned_20k_2ep")
+model.push_to_hub("kalpkanungo/marian-en-de")
+tokenizer.push_to_hub("kalpkanungo/marian-en-de")
+print("EN->DE pushed successfully")
+
+# Push base Helsinki DE->EN model (since you haven't fine-tuned it yet)
+model = MarianMTModel.from_pretrained("Helsinki-NLP/opus-mt-de-en")
+tokenizer = MarianTokenizer.from_pretrained("Helsinki-NLP/opus-mt-de-en")
+model.push_to_hub("kalpkanungo/marian-de-en")
+tokenizer.push_to_hub("kalpkanungo/marian-de-en")
+print("DE->EN pushed successfully")
